de370f97cd519740d8b5f1fd9bc665bfe40d1f11
lazy-devil <lazy-devil@work>
2016-08-24 08:10:36 +0700

    Init

---
+++ PingHost/PingHost/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PingHost
+{
+    partial class Form_Ping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.button_Ping = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(38, 12);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(153, 20);
+            this.textBox_IP.TabIndex = 0;
+            this.textBox_IP.Text = "127.0.0.1";
+            // 
+            // button_Ping
+            // 
+            this.button_Ping.Location = new System.Drawing.Point(197, 10);
+            this.button_Ping.Name = "button_Ping";
+            this.button_Ping.Size = new System.Drawing.Size(75, 23);
+            this.button_Ping.TabIndex = 1;
+            this.button_Ping.Text = "Ping";
+            this.button_Ping.UseVisualStyleBackColor = true;
+            this.button_Ping.Click += new System.EventHandler(this.button_Ping_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Enabled = false;
+            this.progressBar.Location = new System.Drawing.Point(0, 51);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(358, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 3;
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Enabled = false;
+            this.button_Stop.Location = new System.Drawing.Point(278, 10);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(75, 23);
+            this.button_Stop.TabIndex = 1;
+            this.button_Stop.Text = "Stop";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Location = new System.Drawing.Point(35, 35);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(35, 13);
+            this.label_Status.TabIndex = 4;
+            this.label_Status.Text = "status";
+            // 
+            // Form_Ping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 74);
+            this.Controls.Add(this.label_Status);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Ping);
+            this.Controls.Add(this.textBox_IP);
+            this.Name = "Form_Ping";
+            this.Text = "Ping Host";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Ping_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.Button button_Ping;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Label label_Status;
+    }
+}
+
---
+++ PingHost/PingHost/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PingHost
+{
+    public partial class Form_Ping : Form
+    {
+        private BackgroundWorker m_BackgroundWorker = new BackgroundWorker();
+
+        public Form_Ping()
+        {
+            InitializeComponent();
+
+            m_BackgroundWorker = new BackgroundWorker { WorkerReportsProgress = true, WorkerSupportsCancellation = true };
+            m_BackgroundWorker.DoWork += M_BackgroundWorker_DoWork;
+            m_BackgroundWorker.ProgressChanged += M_BackgroundWorker_ProgressChanged;
+            m_BackgroundWorker.RunWorkerCompleted += M_BackgroundWorker_RunWorkerCompleted;
+
+            progressBar.Hide();
+        }
+
+        private void M_BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //MessageBox.Show("Done", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            label_Status.Text = "status";
+            progressBar.Hide();// .Enabled = false;
+            button_Ping.Enabled = true;
+            button_Stop.Enabled = false;
+            textBox_IP.Enabled = true;
+        }
+
+        private void M_BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //progressBar.Value = e.ProgressPercentage;
+            if (e.UserState != null)
+            {
+                var status = (IPStatus)e.UserState;
+                label_Status.Text = string.Format("{0:yyyy-MM-dd HH:mm:ss:fff} {1}", DateTime.Now, status);
+            }
+        }
+
+        private void M_BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var bg = sender as BackgroundWorker;
+            var host = e.Argument.ToString();
+            var path = Path.Combine(Environment.CurrentDirectory, DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".log");
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                try
+                {
+                    sw.WriteLine("{0:yyyy-MM-dd HH:mm:ss:fff} Ping {1}: Start", DateTime.Now, host);
+
+                    var d = DateTime.MinValue;
+                    double i = 0.0;
+                    double max = 100.0;
+                    while (!e.Cancel)
+                    {
+                        var dt = DateTime.Now.AddSeconds(-3);
+                        if (dt.CompareTo(d) > 0)
+                        {
+                            d = DateTime.Now;
+                            var ret = Ping(host);
+                            e.Result = ret;
+                            sw.WriteLine("{0:yyyy-MM-dd HH:mm:ss:fff} Ping {1}: {2}", DateTime.Now, host, ret);
+                            sw.Flush();
+
+                            var p = (int)((i / max) * 100);
+                            bg.ReportProgress(p, ret);
+
+                            i++;
+                            if (i > 100)
+                            {
+                                i = 0;
+                            }
+                        }
+
+                        if (bg.CancellationPending)
+                        {
+                            e.Cancel = true;
+                        }
+
+                        Thread.Sleep(50);
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    sw.WriteLine("{0:yyyy-MM-dd HH:mm:ss:fff} Ping {1}: Finish", DateTime.Now, host);
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+
+        private IPStatus Ping(string hostName, int timeout = 5000)
+        {
+            using (Ping ping = new Ping())
+            {
+                try
+                {
+                    PingOptions option = new PingOptions(126, true);
+                    var buffer = Encoding.ASCII.GetBytes("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+                    PingReply reply = ping.Send(hostName, timeout, buffer, option);
+                    return reply.Status;
+                }
+                catch (Exception)
+                {
+                    return IPStatus.Unknown;
+                }
+            }
+        }
+
+
+        private void button_Ping_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox_IP.Text))
+            {
+                return;
+            }
+
+            if (!m_BackgroundWorker.IsBusy)
+            {
+                var ip = textBox_IP.Text.Trim();
+                m_BackgroundWorker.RunWorkerAsync(ip);
+                progressBar.Show();//.Enabled = true;
+                button_Ping.Enabled = false;
+                button_Stop.Enabled = true;
+                textBox_IP.Enabled = false;
+            }
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            if (m_BackgroundWorker.IsBusy)
+            {
+                m_BackgroundWorker.CancelAsync();
+            }
+        }
+
+        private void Form_Ping_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (m_BackgroundWorker.IsBusy)
+            {
+                m_BackgroundWorker.CancelAsync();
+                while (m_BackgroundWorker.IsBusy)
+                {
+                    Application.DoEvents();
+                    Thread.Sleep(50);
+                }
+            }
+        }
+    }
+}
